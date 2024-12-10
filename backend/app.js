2d02b96aa2dbad2754b59f8bdd2d583348a2eb81
lamux04 <javilabra250604@gmail.com>
2024-12-10 20:06:07 +0100

    .

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     // Todos los origenes
-    origin: 'http://www.sched4all.site',
+    origin: 'true',
     credentials: true
 }))
 app.disable('x-powered-by')
