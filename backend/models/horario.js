6a7809a08346bf7f7c27bf96ab3e32fa3c0387af
lamux04 <javilabra250604@gmail.com>
2024-12-11 12:07:09 +0100

    .

--- backend/models/horario.js
+++ backend/models/horario.js
@@ -93,7 +93,6 @@ export class HorarioModel
             }
             horario["carreras"].push(carrera)
         }
-        console.log(horario)
         return { horario, exists: rows.length > 0 }
     }
 
