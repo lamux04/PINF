328b5afc6b7a43c001a9adce9ff300bf533c8ac9
lamux04 <javilabra250604@gmail.com>
2024-12-11 12:42:35 +0100

    .

--- backend/models/horario.js
+++ backend/models/horario.js
@@ -77,7 +77,6 @@ export class HorarioModel
                     {
                         // const { clase_generada, exists } = await ClaseModel.getClaseGenerada({ clase_cod: clase["codigo"], horar_cod: horario["codigo"] })
                         const clase_generada = clases_obj[clase["codigo"]]
-                        if (!clases_obj[clase["codigo"]]) continue
                         clase["clase_gen_cod"] = clase_generada["codigo"]
                         clase["clase_gen_hinicio"] = clase_generada["hinicio"]
                         clase["clase_gen_hfin"] = clase_generada["hfin"]
