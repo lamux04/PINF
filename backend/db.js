8c96a3dc93492d7f7ade1a9160be41a3f9471cb3
lamux04 <javilabra250604@gmail.com>
2024-12-12 21:06:54 +0100

    .

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,14 @@
 import mysql from 'mysql2'
 import dotenv from 'dotenv'
-import fs from 'fs'
 
 dotenv.config()
 
-const sslCert = fs.readFileSync('./ca.pem')
-
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: 26547,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {
-        ca: sslCert
-    }
 })
 
 export const promisePool = pool.promise()
