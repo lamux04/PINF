ca4e56e4a0ec27bbb80424d630caa836b9fc2516
lamux04 <javilabra250604@gmail.com>
2024-12-12 14:50:54 +0100

    .

--- backend/controllers/horario.js
+++ backend/controllers/horario.js
@@ -3,7 +3,7 @@ import { AuthModel } from '../models/auth.js'
 import { PlantillaModel } from '../models/plantilla.js'
 import { json } from 'express'
 
-const endpoint = 'http://10.182.111.71:8084/sched4allAPI'
+const endpoint = 'https://www.shed4all.site/sched4allAPI'
 
 export class HorarioController
 {
--- frontend/src/variables.js
+++ frontend/src/variables.js
@@ -1,1 +1,1 @@
-export const host =  'http://localhost:1234'
+export const host =  'https://www.sched4all.site'
