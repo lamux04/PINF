14b14fc4c7877df340a89db4dab3c6944b0caa84
lamux04 <javilabra250604@gmail.com>
2024-12-17 11:34:45 +0100

    .

--- frontend/src/routes/editar_curso/components/NombreAsignaturaEditable.jsx
+++ frontend/src/routes/editar_curso/components/NombreAsignaturaEditable.jsx
@@ -17,7 +17,7 @@ export const NombreAsignaturaEditable = ({ asignatura, curso, setCurso}) => {
         {
             setValidacion('El nombre no puede estar vacío')
             setTimeout(() => setValidacion(''), 10000)
-        } else if (nombre.length > 30) {
+        } else if (nombre.length > 60) {
             setValidacion('El nombre no puede tener más de 30 caracteres')
             setTimeout(() => setValidacion(''), 10000)
         } else if (curso.asignaturas.find(el => el.codigo !== asignatura.codigo && el.nombre === nombre)) {
