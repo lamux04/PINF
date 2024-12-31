5394aacf5de0a51e036ac639faba150dd748e53e
JavierGMESA <javier.garmesa@alum.uca.es>
2024-12-31 12:41:16 +0100

    coment

--- Código python/API.py
+++ Código python/API.py
@@ -9,14 +9,14 @@ app = Bottle()
 @app.post("/sched4allAPI/")
 def insertar():
     try:
-        cuerpo = request.body.read()
-        if not cuerpo:
+        cuerpo = request.body.read()                            #Se obtiene el mensaje
+        if not cuerpo:                                          #En caso de mensaje vacío, se devuelve error
             response.status = 400
             return json.dumps({"Error": "Json vacio"})
 
-        datos = json.loads(cuerpo)
-        # print(datos)                        #DEBUG
-    except:
+        datos = json.loads(cuerpo)                              #Se obtiene el JSON en un dicccionario local
+        # print(datos)                                          #DEBUG
+    except:                                                     #En caso de excepción se devuelve mensaje de error
         response.headers["Content-Type"] = "application/json"
         response.status = 404
 
@@ -29,19 +29,13 @@ def insertar():
     tipos_aulas = list(datos["aulas"].keys())       #Obtenemos los tipos de aulas
     for tipo in tipos_aulas:
         aulas_horario[tipo] = []                    #Inicializamos a lista vacía
-
-        #print("---------------------- DICCIONARIO AULAS ------------------------")
-        
         for a in datos["aulas"][tipo]:
-            aulas_horario[tipo].append(a)
+            aulas_horario[tipo].append(a)           #Vamos añadiendo las aulas
 
     #Creamos los profesores
     profesores = {}                                 #Diccionario donde la clave es el nombre del profesor
     for profe in datos["profesores"]:               #y el valor es el objeto
-
-        #print("---------------------- DICCIONARIO PROFESORES ------------------------")
-
-        profesores[profe] = profesor(profe)
+        profesores[profe] = profesor(profe)         #Vamos añadiendo los profesores
 
     Horarios = {}
     clases = []
@@ -52,55 +46,35 @@ def insertar():
     Inicio_desc = int(datos["inicio_desc"])
     Fin_desc = int(datos["fin_desc"])
 
-    #print("---------------------- DICCIONARIO CARRERA ------------------------")
-
+    #Creamos las clases para el generador
     for Carre in datos["carrera"]:                  #Carrera por carrera
 
         carrera_actual = carrera(Carre["nombre"])
-
-        #print("---------------------- DICCIONARIO CURSOS ------------------------")
 
         dict_curso = {}
 
         for Cur in Carre["cursos"]:                     #Curso por curso
 
-            #print("---------------------- CONSTRUYE HORARIO CARRERA ------------------------")
-
-            #print("---------------------- CONSTRUYE HORARIO CARRERA 1 ------------------------")
-
             curso_actual = curso(Cur["nombre"], carrera_actual)
             curso_actual.mostrar()
 
-            #print("---------------------- CONSTRUYE HORARIO CARRERA 2 ------------------------")
-
-            
             dict_curso[curso_actual.nombre] = [[],[],[],[],[]]
-
-            #dict_carrera[carrera_actual.nombre] = dict_curso    #Horario vacío para el curso
-            #Horarios[carrera_actual.nombre] = dict_curso    #Horario vacío para el curso
-
-            #print("---------------------- CONSTRUYE HORARIO CARRERA 3 ------------------------")
 
             asignaturas =  Cur["asignaturas"]    #Asignaturas del curso
             for asig in asignaturas:
                 asignatura_actual = asignatura(asig["nombre"], asig["aprobable"], curso_actual)     #Creamos asignatura
                 clases_asignatura = asig["clases"]
 
-                #print("---------------------- CONSTRUYE CLASES ASIGNATURA ------------------------")
-
-                for clas in clases_asignatura:
+                for clas in clases_asignatura:          #Vamos creando las clases de cada asignatura
                     clase_actual = clase(clas["nombre"], clas["tipo"], clas["tipo_aula"], int(clas["duracion"]), clas["importante"], asignatura_actual, profesores[clas["profesor"]])   #Creamos clase
                     clases.append(clase_actual)
         
         Horarios[carrera_actual.nombre] = dict_curso
 
-    #print(Horarios)
-
-
-    # v2.colocarClases(clases, aulas_horario, Horarios)
+    #Generamos el horario
     try:
-        v3.colocarClases(clases, aulas_horario, Horarios, H_inicio, H_final, Inicio_desc, Fin_desc)
-    except:
+        v3.colocarClases(clases, aulas_horario, Horarios, H_inicio, H_final, Inicio_desc, Fin_desc)     #Se genera el horario
+    except:                                                                                             #En caso de error al generar se devuelve un código de error
         response.headers["Content-Type"] = "application/json"
         response.status = 400
 
@@ -108,35 +82,35 @@ def insertar():
 
         return json.dumps({"Error": "Error de generacion"})
     
+    #Construimos el JSON a enviar
     l2: c_horario
-
     Respuesta = {}
     #n_carreras = 0
     carreras = list(Horarios.keys())
     Respuesta["carrera"] = []
     for i in range(len(carreras)):          #Carrera por carrera
         carre = {
-            "nombre": carreras[i]
+            "nombre": carreras[i]           #Añadimos nombre de la carrera al JSON
             }
         Respuesta["carrera"].append(carre)
         cursos = list(Horarios[carreras[i]].keys())
         Respuesta["carrera"][i]["cursos"] = []
         for j in range(len(cursos)):        #Curso por curso
             cur = {
-                "nombre": cursos[j]
+                "nombre": cursos[j]         #Añadimos nombre del curso al JSON
                 }
             Respuesta["carrera"][i]["cursos"].append(cur)
             Respuesta["carrera"][i]["cursos"][j]["clases"] = [[],[],[],[],[]]       #Campo del JSON inicialmente vacío
             for k in range(5):                                                      #Día por día
                 if len(Horarios[carreras[i]][cursos[j]][k]) != 0:
-                    if type(Horarios[carreras[i]][cursos[j]][k][0]) is list:
+                    if type(Horarios[carreras[i]][cursos[j]][k][0]) is list:            #En caso de haber más de una lista de clases por día
                         Respuesta["carrera"][i]["cursos"][j]["clases"][k] = [[]]
                         for l1 in range(len(Horarios[carreras[i]][cursos[j]][k])):      #l1 va desde 0 hasta número_de_listas_del_dia - 1
                             #Respuesta["carrera"][i]["cursos"][j]["clases"][k][l1] = []
                             if l1 > 0:
                                 Respuesta["carrera"][i]["cursos"][j]["clases"][k].append([])
                             for l2 in Horarios[carreras[i]][cursos[j]][k][l1]:
-                                classe = {
+                                classe = {                                              #Vamos creando los horarios del JSON a enviar
                                     "nombre": l2.clase.nombre,
                                     "h_ini": l2.h_ini,
                                     "h_fin": l2.h_fin,
@@ -149,7 +123,7 @@ def insertar():
 
                     else:
                         for l2 in Horarios[carreras[i]][cursos[j]][k]:
-                            classe = {
+                            classe = {                                                  #Vamos creando los horarios del JSON a enviar
                                     "nombre": l2.clase.nombre,
                                     "h_ini": l2.h_ini,
                                     "h_fin": l2.h_fin,
@@ -161,7 +135,7 @@ def insertar():
     
     response.status = 200
     response.headers["Content-Type"] = "application/json"
-    return json.dumps(Respuesta)
+    return json.dumps(Respuesta)                                            #Se devuelve el JSON completo
 
 if __name__ == "__main__":
     run(app, host='localhost', port=8084)
